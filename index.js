d33d88b9da28498140be4ac35d3cb5d92b34d8c4
anuptiwari <anupwork26@gmail.com>
2026-03-17 01:21:32 +0530

    devops updated

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
-
 app.get("/", (req, res) => {
-  res.send("hi baby i am a how are you ");
+  res.send(" DevOps engineer doesn't sleep, he waits for build to finish 😴",
+    "Works on my machine 😎",
+    "Did you try turning it off and on again? 🔌  ");
 });
 
 app.listen(3000, () => {
